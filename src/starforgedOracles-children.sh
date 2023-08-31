a57119279c3a4a267bcbc61c3af65994cab199b8
David.Hudson <david.hudson@disney.com>
2023-08-30 17:17:20 -0700

    Add entries to planet folders

--- src/starforgedOracles-children.sh
+++ src/starforgedOracles-children.sh
@@ -15,6 +15,26 @@
 # [12] Starships aO8e4enqhqWw25H6
 # [13] Vaults N2sObuKzWFZ5LEU2
 
+oracle_folder_parser () {
+    cat << EOF
+    starforgedOracles.children[$folder_index].children.push({
+          displayName: '$name',
+          tables: ['Compendium.starsmith-expanded-oracles.starsmithexpandedoracles.RollTable.$id'],
+          children: []
+      })
+EOF
+}
+
+oracle_subfolder_parser () {
+    cat << EOF
+    starforgedOracles.children[$folder_index].children[$sub_folder_index].children.push({
+          displayName: '$name',
+          tables: ['Compendium.starsmith-expanded-oracles.starsmithexpandedoracles.RollTable.$id'],
+          children: []
+      })
+EOF
+}
+
 jq -c '.items[] | {name,_id,folder}' starsmith-expanded-oracles-export.json | while read oracleJSON;
 do
     name=$(echo $oracleJSON | jq -r .name)
@@ -22,66 +42,127 @@ do
     id=$(echo $oracleJSON | jq -r ._id)
     folder=$(echo $oracleJSON | jq -r .folder)
     #echo $id
-    
+
     case $folder in
         Uylg7OA7KprzNn9j)
-            folder_index=0
+            folder_index="characterCreationIndex"
+            oracle_folder_parser
             ;;
         EDUYGoTtXEMkKKvh)
-            folder_index=1
+            folder_index="charactersIndex"
+            oracle_folder_parser
             ;;
         SW9eKiLhIBf4c0Km)
-            folder_index=2
+            folder_index="coreIndex"
+            oracle_folder_parser
             ;;
         RSG7ksc1DxCTpccr)
-            folder_index=3
+            folder_index="creaturesIndex"
+            oracle_folder_parser
             ;;
         uFBHcawqNPAmWvqe)
-            folder_index=4
-            ;;
-        h07LKcgdKeSsWGQ2)
-            folder_index=5
-            ;;
-        HhkNFM4BelwaXHfB)
-            folder_index=6
-            ;;
-        EZdpqOZMdqwGsU7q)
-            folder_index=7
-            ;;
-        6cVVQY9tOQiVInzx)
-            folder_index=8
-            ;;
-        xJpOuGNKKjAVrR3q)
-            folder_index=9
-            ;;
-        M0bP1WUZxPqCOHJS)
-            folder_index=10
-            ;;
-        9aKg1x9pvKGwheVZ)
-            folder_index=11
-            ;;
-        aO8e4enqhqWw25H6)
-            folder_index=12
-            ;;
-        N2sObuKzWFZ5LEU2)
-            folder_index=13
+            folder_index="derelictsIndex"
+            oracle_folder_parser
             ;;
         zBzW0zEkqjmtSZrH)
             # Districts
-            folder_index=4
+            folder_index="districtsIndex"
+            oracle_folder_parser
+            ;;
+        h07LKcgdKeSsWGQ2)
+            folder_index="factionsIndex"
+            oracle_folder_parser
+            ;;
+        HhkNFM4BelwaXHfB)
+            folder_index="locationThemesIndex"
+            oracle_folder_parser
+            ;;
+        EZdpqOZMdqwGsU7q)
+            folder_index="miscIndex"
+            oracle_folder_parser
+            ;;
+        6cVVQY9tOQiVInzx)
+            folder_index="movesIndex"
+            oracle_folder_parser
+            ;;
+        V2NII2gORVPEdj28)
+            folder_index="planetsIndex"
+            oracle_folder_parser
+            ;;
+        M0bP1WUZxPqCOHJS)
+            folder_index="settlementsIndex"
+            oracle_folder_parser
+            ;;
+        9aKg1x9pvKGwheVZ)
+            folder_index="spaceIndex"
+            oracle_folder_parser
+            ;;
+        aO8e4enqhqWw25H6)
+            folder_index="starshipsIndex"
+            oracle_folder_parser
+            ;;
+        N2sObuKzWFZ5LEU2)
+            folder_index="vaultsIndex"
+            oracle_folder_parser
+            ;;
+        Pl9XxW6fIhAGfABi)
+            folder_index="planetsIndex"
+            sub_folder_index="desertPlanetIndex"
+            oracle_subfolder_parser
+            ;;
+        Xx3Lo4c3NF2M0ymk)
+            folder_index="planetsIndex"
+            sub_folder_index="furnacePlanetIndex"
+            oracle_subfolder_parser
+            ;;
+        EJoSoFU1mGwf60NT)
+            folder_index="planetsIndex"
+            sub_folder_index="gravePlanetIndex"
+            oracle_subfolder_parser
+            ;;
+        NxLWkP3u1WpG2vuX)
+            folder_index="planetsIndex"
+            sub_folder_index="icePlanetIndex"
+            oracle_subfolder_parser
+            ;;
+        FmcXyPfargJoxCMr)
+            folder_index="planetsIndex"
+            sub_folder_index="jovianPlanetIndex"
+            oracle_subfolder_parser
+            ;;
+        khXSFYiPdcUTBfbE)
+            folder_index="planetsIndex"
+            sub_folder_index="junglePlanetIndex"
+            oracle_subfolder_parser
+            ;;
+        tl9BYoquB8rsJXih)
+            folder_index="planetsIndex"
+            sub_folder_index="oceanPlanetIndex"
+            oracle_subfolder_parser
+            ;;
+        F2UgfneAghjUe29m)
+            folder_index="planetsIndex"
+            sub_folder_index="rockyPlanetIndex"
+            oracle_subfolder_parser
+            ;;
+        v5cvqSu1q6CUcfPg)
+            folder_index="planetsIndex"
+            sub_folder_index="shatteredPlanetIndex"
+            oracle_subfolder_parser
+            ;;
+        kr3MLzciAv6E5cjr)
+            folder_index="planetsIndex"
+            sub_folder_index="taintedPlanetIndex"
+            oracle_subfolder_parser
+            ;;
+        xJpOuGNKKjAVrR3q)
+            folder_index="planetsIndex"
+            sub_folder_index="vitalPlanetIndex"
+            oracle_subfolder_parser
             ;;
         *)
-            folder_index=9
+            folder_index="planetsIndex"
+            oracle_folder_parser
             ;;
     esac
-
-    
-
-    cat << EOF
-    starforgedOracles.children[$folder_index].children.push({
- 	  displayName: '$name',
- 	  tables: ['Compendium.starsmith-expanded-oracles.starsmithexpandedoracles.RollTable.$id'],
- 	  children: []
- 	})
-EOF
 done
